7cb00303dcece50f1b54acc3dbd3954c21d43e00
Nandan Nagane <np113218@gmail.com>
2026-08-23 20:03:37 +0530

    Resume preview update

--- src/config/Resume.ts
+++ src/config/Resume.ts
@@ -1,3 +1,3 @@
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/1FHHPVbqHlVHVA8vH2Wo45ZyCR5PeUYzn/preview',
+  url: 'https://drive.google.com/file/d/1lh71sXdFhvLf_QmBNM2m-SbQ1R2Q4dE8/preview',
 };
